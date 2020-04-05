3ecda33ef96455b3dc971b0bd6c78bf35a2dbed5
fatih günalp <fatih.gunalp@outlook.com>
2020-04-05 19:35:42 +0300

    .

---
+++ MVC/05.04/MVC/MVCLayout/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLayout.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
