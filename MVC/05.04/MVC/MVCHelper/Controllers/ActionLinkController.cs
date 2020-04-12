f8bb1263c5f1cb2d4640f5f16be2d100e71754a1
fatih günalp <fatih.gunalp@outlook.com>
2020-04-12 16:49:34 +0300

    .

---
+++ MVC/05.04/MVC/MVCHelper/Controllers/ActionLinkController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelper.Controllers
+{
+    public class ActionLinkController : Controller
+    {
+        // GET: ActionLink
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC/05.04/MVC/MVCTekrar/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTekrar.Models;
+
+namespace MVCTekrar.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            List<Category> categories = new List<Category>()
+            {
+                new Category{ID=1,CategoryName="Yiyecek"},
+                new Category{ID=2,CategoryName="İçecek"},
+                new Category{ID=3,CategoryName="Giyim"}
+
+            };
+            return View(categories);
+        }
+
+        [HttpPost]
+        public ViewResult Index(string kategoriAdi)
+        {
+            //burada kategoriAdi yakalandıktan sonra istediğimiz işlemleri gerçekleştirebiliriz.
+            return View();
+        }
+
+
+    }
+}
