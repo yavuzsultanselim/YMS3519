fcd9eaff8dbcc130c82c7b8712754af765a2c119
fatih günalp <fatih.gunalp@outlook.com>
2020-04-12 18:53:13 +0300

    .

---
+++ MVC/05.04/MVC/MVCHelperPostData/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using MVCHelperPostData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelperPostData.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            List<Product> products = db.Products.OrderByDescending(x=>x.ProductID).ToList();
+            return View(products);
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+    }
+}
