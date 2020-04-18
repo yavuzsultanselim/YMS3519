91718467093e59806eaf070580a3d6f02eb754fe
fatih günalp <fatih.gunalp@outlook.com>
2020-04-18 18:56:58 +0300

    .

---
+++ MVC/05.04/MVC/MVC_Bundles/App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MVC_Bundles.App_Start
+{
+    public class BundleConfig
+    {
+        //Static bir metot tanımlamalıyız.
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundle/css").Include(
+               "~/Content/plugins/fontawesome-free/css/all.min.css",
+               "~/Content/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css",
+               "~/Content/plugins/icheck-bootstrap/icheck-bootstrap.min.css",
+               "~/Content/plugins/jqvmap/jqvmap.min.css",
+               "~/Content/dist/css/adminlte.min.css",
+               "~/Content/plugins/overlayScrollbars/css/OverlayScrollbars.min.css",
+               "~/Content/plugins/daterangepicker/daterangepicker.css",
+               "~/Content/plugins/summernote/summernote-bs4.css"
+                ));
+            bundles.Add(new ScriptBundle("~/bundle/js").Include(
+                "~/Content/plugins/jquery/jquery.min.js",
+                "~/Content/plugins/jquery-ui/jquery-ui.min.js",
+                "~/Content/plugins/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/plugins/chart.js/Chart.min.js",
+                "~/Content/plugins/sparklines/sparkline.js",
+                "~/Content/plugins/jqvmap/jquery.vmap.min.js",
+                "~/Content/plugins/jqvmap/maps/jquery.vmap.usa.js",
+                "~/Content/plugins/jquery-knob/jquery.knob.min.js",
+                "~/Content/plugins/moment/moment.min.js",
+                "~/Content/plugins/daterangepicker/daterangepicker.js",
+                "~/Content/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js",
+                "~/Content/plugins/summernote/summernote-bs4.min.js",
+                "~/Content/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js",
+                "~/Content/dist/js/adminlte.js",
+                "~/Content/dist/js/pages/dashboard.js",
+                "~/Content/dist/js/demo.js"
+                ));
+        }
+    }
+
+}
--- MVC/05.04/MVC/MVC_Bundles/Global.asax.cs
+++ MVC/05.04/MVC/MVC_Bundles/Global.asax.cs
@@ -1,8 +1,10 @@
+using MVC_Bundles.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MVC_Bundles
@@ -13,6 +15,7 @@ namespace MVC_Bundles
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
