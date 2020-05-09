1996a047398bc6f8c597e4bed2a4a2edd1f29fe6
fatih günalp <fatih.gunalp@outlook.com>
2020-05-09 19:17:19 +0300

    .

---
+++ API/NetWebApi/NetWebApi/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NetWebApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        #region API Nedir?
+        /*
+         SOA (Service Oriented Architecture)
+
+--Servis odaklı mimari veya diğer tanımlaması ile hizmet yönelimli mimari bilgisayarlardaki işlevselliklerini iş süreçleri etrafında gruplayarak sistem geliştirilmesine yol gösteren bir yazılım tasarım felsefesidir.
+
+Web Service 
+
+Http protokolü üzerinden xml veya json medya tipleri ile uzak cihazlar arasındaki iletişimi saülayan bir haberleşme yöntemidir.
+
+
+API (Application Programming Interface)
+
+bir yazılımın baika bir yazılımda tanımlanmış işlevlerini kullanabilmesi için oluşturulmuş bir tanım bütünüdür.
+
+//Apinin belli başlı özellikleri
+*GET,POST,PUT,DELETE gibi HTTP metotlarını destekler.
+
+             */ 
+        #endregion
+
+        public static List<string> sampleProducts = new List<string>()
+        {
+            "Chai",
+            "Chang",
+            "Ikura"
+        };
+        //localhost:1222/Home
+
+            //Api istekleri varsayılan olarak GET,POST,PUT,DELETE isimli metotları arar. o yüzden metot isimleri önemli.
+        public IEnumerable<string> GET()
+        {
+            return sampleProducts;
+        }
+
+        public string Get(int id)
+        {
+            return sampleProducts[id];
+        }
+
+        public void Post([FromBody]string value)
+        {
+            sampleProducts.Add(value);
+        }
+
+        public void Put(int id,[FromBody] string value)//2
+        {
+            sampleProducts[id] = value;
+        }
+
+        public void Delete(int id)
+        {
+            sampleProducts.Remove(sampleProducts[id]);
+        }
+
+    }
+}
---
+++ API/NetWebApiStudent/NetWebApiStudent/Controllers/StudentController.cs
@@ -0,0 +1,94 @@
+﻿using NetWebApiStudent.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NetWebApiStudent.Controllers
+{
+    public class StudentController : ApiController
+    {
+        StudentDBEntities db = new StudentDBEntities();
+
+        public IEnumerable<Student> Get()
+        {
+            return db.Students.ToList();
+        }
+
+        //public Student Get(int id)
+        //{
+        //    Student student = db.Students.FirstOrDefault(x => x.ID == id);
+        //    return student;
+        //}
+
+        //HttpResponseMessage 
+        /*
+         HttpResponseMessage döndüren metotlar gelen isteğin fiiline göre Get,Post,Put,Delete ile beraber kullanılırlar. Kaynağa ulaşıldığı takdirde HttpStatus Code ve httpresponse message olarak xml ve json formatlarından biri halinde veri istemciye gönderilir.
+         */
+
+        public HttpResponseMessage Get(int id)//6
+        {
+            Student student = db.Students.FirstOrDefault(x => x.ID == id);
+            if (student != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, student);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"{id} numaralı bir öğrenci bulunamadı.");
+            }
+        }
+
+        //Post
+        //parametre boşsa 400 hata mesajını dönsün.
+        public HttpResponseMessage Post(Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.Created, student);
+        }
+
+        //Put
+        //parametrede bulunan id boşsa geriye 404 dolu ise ve güncelleme başarılıysa 200 mesajını dönsün.
+
+        public HttpResponseMessage Put(int id,Student student)
+        {
+            Student updated = db.Students.FirstOrDefault(x => x.ID == id);
+            if (updated != null)
+            {
+                updated.FirstName = student.FirstName;
+                updated.LastName = student.LastName;
+                updated.Gender = student.Gender;
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, $"{id} numaralı öğrenci güncellendi.");
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"{id} numaralı bir öğrenci bulunamadı");
+            }
+        }
+
+        //Delete
+        //parametrede bulunan id boşsa geriye 404 dolu ise ve güncelleme başarılıysa 200 mesajını dönsün.
+        public HttpResponseMessage Delete(int id)//5
+        {
+          Student deleted=  db.Students.Find(id);
+            if (deleted != null)
+            {
+                db.Students.Remove(deleted);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"{id} numaralı bir öğrenci bulunamadı");
+            }
+        }
+
+        
+
+    }
+}
